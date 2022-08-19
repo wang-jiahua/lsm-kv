fac29eab58dde44afc4890a216a7889049789982
jhwang <jiahua-wang@outlook.com>
2022-08-19 22:23:56 +0800

    test: add bloom test

--- test/test.h
+++ test/test.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 
-#include "kvstore.h"
+#include "../kvstore.h"
 
 class Test {
 protected:
---
+++ test/test_bloom.cc
@@ -0,0 +1,83 @@
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+#include "../bloom.h"
+#include "test.h"
+
+class BloomTest : public Test {
+private:
+    const uint64_t SIMPLE_TEST_MAX = 512;
+    const uint64_t MIDDLE_TEST_MAX = 1024 * 64;
+    const uint64_t LARGE_TEST_MAX = 1024 * 1024 * 8;
+
+    BloomFilter bloomFilter{};
+
+    void regular_test(uint64_t max) {
+        uint64_t i;
+
+        // Test a single key
+        EXPECT(false, bloomFilter.contains(1));
+        bloomFilter.add(1);
+        EXPECT(true, bloomFilter.contains(1));
+        bloomFilter.reset();
+        EXPECT(false, bloomFilter.contains(1));
+
+        phase();
+
+        // Test multiple additions
+        for (i = 0; i < max; ++i) {
+            bloomFilter.add(i);
+            EXPECT(true, bloomFilter.contains(i));
+        }
+        phase();
+
+        // Test after all additions
+        for (i = 0; i < max; ++i)
+            EXPECT(true, bloomFilter.contains(i));
+        phase();
+
+        bloomFilter.reset();
+
+        // Test after reset
+        for (i = 0; i < max; i += 1)
+            EXPECT(false, bloomFilter.contains(i));
+        phase();
+
+        report();
+    }
+
+public:
+    explicit BloomTest(const std::string &dir, bool v = true)
+            : Test(dir, v) {
+    }
+
+    void start_test(void *args = nullptr) override {
+        std::cout << "Bloom filter Test" << std::endl;
+
+        std::cout << "[Simple Test]" << std::endl;
+        regular_test(SIMPLE_TEST_MAX);
+
+        std::cout << "[Middle Test]" << std::endl;
+        regular_test(MIDDLE_TEST_MAX);
+
+        std::cout << "[Large Test]" << std::endl;
+        regular_test(LARGE_TEST_MAX);
+    }
+};
+
+int main(int argc, char *argv[]) {
+    bool verbose = (argc == 2 && std::string(argv[1]) == "-v");
+
+    std::cout << "Usage: " << argv[0] << " [-v]" << std::endl;
+    std::cout << "  -v: print extra info for failed tests [currently ";
+    std::cout << (verbose ? "ON" : "OFF") << "]" << std::endl;
+    std::cout << std::endl;
+    std::cout.flush();
+
+    BloomTest test(".", verbose);
+
+    test.start_test();
+
+    return 0;
+}
